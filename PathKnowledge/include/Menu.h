d9d14d7f15876255e33a992bafa101b06c0beea7
Monica <nika.gonz6996@gmail.com>
2019-05-16 15:17:07 +0200

    Menu bonito

--- PathKnowledge/include/Menu.h
+++ PathKnowledge/include/Menu.h
@@ -11,21 +11,37 @@ class Menu
     public:
         Menu();
         virtual ~Menu();
-        int menuInicio(sf::RenderWindow* window);
-        int menuPause(sf::RenderWindow* window);
+        int menuInicio(sf::RenderWindow* w);
+        int menuPause(sf::RenderWindow* w);
 
     protected:
 
     private:
+        sf::RenderWindow* window;
+        int width, height;
+
+        sf::Texture t_fondo;
+        sf::Texture t_marco;
+        sf::Texture t_fondo_b1;
+        sf::Texture t_fondo_b2;
+
+        sf::Sprite marco;
+        sf::Sprite fondo;
+
         int objetoSeleccionado;
         int numObjetos; // numero de objetos en el menu
         sf::Font fuente; // fuente de la letra
         sf::Text* menuObjetos; // objetos del menu
+        sf::Text titulo;
+        sf::Text titulo_sombra;
 
         void moveUp();
         void moveDown();
         void marcarSeleccionado();
-        void dibujar(sf::RenderWindow* window);
+        void dibujar();
+
+        void creditos();
+        void ayuda();
 };
 
 #endif // MENU_H
--- PathKnowledge/src/Menu.cpp
+++ PathKnowledge/src/Menu.cpp
@@ -4,30 +4,83 @@ Menu::Menu()
 {
     objetoSeleccionado = 0;
     numObjetos = 0;
-    if(!fuente.loadFromFile("resources/fonts/Pixeled.ttf"))
+    if(!fuente.loadFromFile("resources/Menus/2.TTF"))
     {
-        cout<<"Error al cargar la fuente"<<endl;
+        cout<<"Error al cargar la fuente del texto. Menu"<<endl;
     }
+    if(!t_fondo.loadFromFile("resources/Menus/fondo1.jpg"))
+    {
+        cout<<"Error al cargar la la textura del fondo. Menu"<<endl;
+    }
+    if(!t_marco.loadFromFile("resources/Menus/marco2.png"))
+    {
+        cout<<"Error al cargar la la textura del fondo. Menu"<<endl;
+    }
+    if(!t_fondo_b1.loadFromFile("resources/Menus/chain.jpg"))
+    {
+        cout<<"Error al cargar la la textura chain. Menu"<<endl;
+    }
+    if(!t_fondo_b2.loadFromFile("resources/Menus/papiro.jpg"))
+    {
+        cout<<"Error al cargar la la textura papiro. Menu"<<endl;
+    }
+    marco = sf::Sprite(t_marco);
+    fondo = sf::Sprite(t_fondo);
+    fondo.scale(1.15,1);
+
+    //marco.setColor(sf::Color(120,80,50));
+    marco.scale(0.3,0.1);
+    marco.setOrigin(marco.getLocalBounds().width/2, marco.getLocalBounds().height/2-110);
     menuObjetos = NULL;
 }
 
-int Menu::menuInicio(sf::RenderWindow* window)
+int Menu::menuInicio(sf::RenderWindow* w)
 {
     // INICIALIZAR VALORES
+    window = w;
+    width = window->getSize().x;
+    height = window->getSize().y;
+
+
+    titulo = sf::Text();
+    titulo.setString("     Path \n\n       of \n\n Knowledge");
+    titulo.setFont(fuente);
+    titulo.setCharacterSize(70);
+    titulo.setColor(sf::Color::Yellow);
+    titulo.setOrigin(0,0);
+    titulo.setPosition(600,150);
+    titulo_sombra = sf::Text();
+    titulo_sombra.setString(titulo.getString());
+    titulo_sombra.setFont(fuente);
+    titulo_sombra.setCharacterSize(70);
+    titulo_sombra.setColor(sf::Color(0,0,0));
+    titulo_sombra.setOrigin(2,2);
+    titulo_sombra.setPosition(titulo.getPosition());
+
     numObjetos = 4;
     menuObjetos = new sf::Text[numObjetos];
-    int width = window->getSize().x;
-    int height = window->getSize().y;
 
-    menuObjetos[0].setString("Comenzar partida");
+    menuObjetos[0].setString("Comenzar");
     menuObjetos[1].setString("Ayuda");
     menuObjetos[2].setString("Creditos");
     menuObjetos[3].setString("Salir");
     for (int i=0; i<numObjetos; i++)
     {
         menuObjetos[i].setFont(fuente);
-        menuObjetos[i].setColor(sf::Color::White);
-        menuObjetos[i].setPosition(sf::Vector2f((width/2)-(menuObjetos[i].getLocalBounds().width/2), height/(numObjetos+1)*(i+1)));
+        menuObjetos[i].setColor(sf::Color(230,230,230));
+        menuObjetos[i].setCharacterSize(40);
+        menuObjetos[i].setOrigin(menuObjetos[i].getLocalBounds().width/2, menuObjetos[i].getLocalBounds().height/2);
+        menuObjetos[i].setPosition(width/2-200, height/(numObjetos+1)*(i+1));
+    }
+    sf::Text* menuObjetos2 = new sf::Text[numObjetos];
+    for (int i=0; i<numObjetos; i++)
+    {
+        menuObjetos2[i].setString(menuObjetos[i].getString());
+        menuObjetos2[i].setFont(fuente);
+        menuObjetos2[i].setColor(sf::Color(30,30,30));
+        menuObjetos2[i].setCharacterSize(40);
+        menuObjetos2[i].setOrigin(menuObjetos[i].getLocalBounds().width/2-1, menuObjetos[i].getLocalBounds().height/2-1);
+        menuObjetos2[i].setPosition(menuObjetos[i].getPosition());
     }
     objetoSeleccionado = 0;
     marcarSeleccionado();
@@ -67,9 +120,11 @@ int Menu::menuInicio(sf::RenderWindow* window)
                                     break;
                                 case 1:
                                     //AYUDA
+                                    ayuda();
                                     break;
                                 case 2:
                                     //CREDITOS
+                                    creditos();
                                     break;
                                 case 3:
                                     //SALIR
@@ -83,28 +138,328 @@ int Menu::menuInicio(sf::RenderWindow* window)
         }
 
         // DIBUJAR
-        dibujar(window);
+        window->clear();
+        window->draw(fondo);
+        window->draw(marco);
+        for (int i=0; i<numObjetos; i++)
+        {
+            window->draw(menuObjetos2[i]);
+        }
+        for (int i=0; i<numObjetos; i++)
+        {
+            window->draw(menuObjetos[i]);
+        }
+        window->draw(titulo_sombra);
+        window->draw(titulo);
+        window->display();
+
     }
     // eliminar puntero
     delete[] menuObjetos;
     menuObjetos = NULL;
+    delete[] menuObjetos2;
+    menuObjetos2 = NULL;
+    estado = NULL;
 }
 
-int Menu::menuPause(sf::RenderWindow* window)
+void Menu::creditos()
+{
+    sf::Text t = sf::Text();
+    t.setString("Desarrolladores:    PentaEnix");
+    t.setFont(fuente);
+    t.setCharacterSize(60);
+    t.setColor(sf::Color::Yellow);
+    t.setOrigin(0,0);
+    t.setPosition(170,100);
+    sf::Text t_sombra = sf::Text();
+    t_sombra.setString(t.getString());
+    t_sombra.setFont(fuente);
+    t_sombra.setCharacterSize(60);
+    t_sombra.setColor(sf::Color::Black);
+    t_sombra.setOrigin(-2,-2);
+    t_sombra.setPosition(t.getPosition());
+
+    int c_lenght = 7;
+    sf::Text* contenido = new sf::Text[c_lenght];
+    contenido[1].setString("Jose Antonio Sansano Melero");
+    contenido[6].setString("Javier Alted Navarro");
+    contenido[5].setString("Gaspar Angel Tortosa Pardo");
+    contenido[4].setString("Monica Iglesias Gonzalez");
+    contenido[3].setString("Jose Manuel Manresa Guirao");
+    contenido[2].setString("Ruben Muriana Sala");
+    for (int i=1; i<c_lenght; i++)
+    {
+        contenido[i].setFont(fuente);
+        contenido[i].setCharacterSize(30);
+        contenido[i].setColor(sf::Color(230,230,230));
+        contenido[i].setOrigin(0,0);
+        contenido[i].setPosition(210, 60*i+170);
+    }
+    sf::Text* contenido2 = new sf::Text[c_lenght];
+    for (int i=1; i<c_lenght; i++)
+    {
+        contenido2[i].setString(contenido[i].getString());
+        contenido2[i].setFont(fuente);
+        contenido2[i].setCharacterSize(30);
+        contenido2[i].setColor(sf::Color(30,30,30));
+        contenido2[i].setOrigin(1,1);
+        contenido2[i].setPosition(210, 60*i+170);
+    }
+
+    sf::Text volver = sf::Text();
+    volver.setString("Volver");
+    volver.setFont(fuente);
+    volver.setCharacterSize(40);
+    volver.setColor(sf::Color(180,0,0));
+    volver.setOrigin(volver.getLocalBounds().width/2, volver.getLocalBounds().height/2);
+    volver.setPosition(800,500);
+    sf::Text volver2 = sf::Text();
+    volver2.setString("Volver");
+    volver2.setFont(fuente);
+    volver2.setCharacterSize(40);
+    volver2.setColor(sf::Color(0,0,0));
+    volver2.setOrigin(volver.getLocalBounds().width/2-1, volver.getLocalBounds().height/2-1);
+    volver2.setPosition(800,500);
+    sf::Sprite m = sf::Sprite(t_marco);
+    m.scale(0.3,0.1);
+    m.setOrigin(marco.getLocalBounds().width/2, marco.getLocalBounds().height/2-110);
+    m.setPosition(volver.getPosition());
+
+
+    bool seguir = true;
+    while (window->isOpen() && seguir)
+    {
+        sf::Event event;
+        while (window->pollEvent(event))
+        {
+            switch(event.type)
+            {
+                case sf::Event::Closed:
+                    window->close();
+                    break;
+                case sf::Event::KeyPressed:
+                // GESTION DE EVENTOS
+                    switch(event.key.code)
+                    {
+                        case sf::Keyboard::Space:
+                        case sf::Keyboard::Return:
+                        case sf::Keyboard::Escape:
+                            // CAMBIAR ESTADO
+                            seguir = false;
+                            break;
+                    }
+                    break;
+            }
+        }
+        window->clear();
+        window->draw(fondo);
+        window->draw(t_sombra);
+        window->draw(t);
+        window->draw(volver2);
+        window->draw(volver);
+        window->draw(m);
+        for(int i=1; i<c_lenght;i++)
+            window->draw(contenido2[i]);
+        for(int i=1; i<c_lenght;i++)
+            window->draw(contenido[i]);
+        window->display();
+    }
+    delete[] contenido;
+    contenido = NULL;
+    delete[] contenido2;
+    contenido2 = NULL;
+}
+void Menu::ayuda()
+{
+    sf::Text t = sf::Text();
+    t.setString("Ayuda");
+    t.setFont(fuente);
+    t.setCharacterSize(60);
+    t.setColor(sf::Color::Yellow);
+    t.setOrigin(t.getLocalBounds().width/2,0);
+    t.setPosition(width/2,100);
+    sf::Text t_sombra = sf::Text();
+    t_sombra.setString(t.getString());
+    t_sombra.setFont(fuente);
+    t_sombra.setCharacterSize(60);
+    t_sombra.setColor(sf::Color(0,0,0));
+    t_sombra.setOrigin(t_sombra.getLocalBounds().width/2-2,0-2);
+    t_sombra.setPosition(t.getPosition());
+
+    int num_im = 3;
+    sf::Texture* textures = new sf::Texture[num_im];
+
+    if(!textures[0].loadFromFile("resources/Menus/wasd2.png"))
+    {
+        cout<<"Error al cargar la textura wasd2. Menu - Ayuda"<<endl;
+    }
+    if(!textures[1].loadFromFile("resources/Menus/intro.png"))
+    {
+        cout<<"Error al cargar la textura intro. Menu - Ayuda"<<endl;
+    }
+    if(!textures[2].loadFromFile("resources/Menus/estres-cono.png"))
+    {
+        cout<<"Error al cargar la textura cono. Menu - Ayuda"<<endl;
+    }
+
+    sf::Sprite* imagenes = new sf::Sprite[num_im];
+    for(int i=0; i<num_im; i++)
+        imagenes[i] = sf::Sprite(textures[i]);
+    imagenes[0].setTextureRect(sf::IntRect(100,100,400,190));
+    imagenes[0].setColor(sf::Color(200,200,200));
+    imagenes[0].setScale(0.5,0.5);
+    imagenes[1].setScale(0.16,0.16);
+    imagenes[2].setScale(0.5,0.5);
+    imagenes[0].setPosition(220,130);
+    imagenes[1].setPosition(700,150);
+    imagenes[2].setPosition(180,290);
+
+
+    int c_lenght = 7;
+    sf::Text* contenido = new sf::Text[c_lenght];
+    contenido[0].setString("Te has embarcado en un trepidante viaje en busca de conocimiento.");
+    contenido[1].setString("Con las teclas 'wasd' te desplazas por el mapa.   Con la tecla 'intro' interactuas con los objetos.");
+    contenido[3].setString("Barra de conocimiento: tu objetivo es aumentarla al maximo.");
+    contenido[2].setString("Barra de estres: cuando se llene perderas conocimiento y volveras al inicio.");
+    contenido[4].setString("Cuando te topes con un enemigo entraras en combate con el. Derrotalo antes de que lo haga el.");
+    contenido[5].setString("El combate se realiza por turnos, atacas tu y luego ataca el enemigo.");
+    contenido[6].setString("Derrota a tus enemigos para conseguir tu titulo universitario.");
+    for (int i=0; i<c_lenght; i++)
+    {
+        contenido[i].setFont(fuente);
+        contenido[i].setCharacterSize(20);
+        contenido[i].setColor(sf::Color(230,230,230));
+        contenido[i].setOrigin(0,0);
+        contenido[i].setPosition(180, 50*i+190);
+    }
+    contenido[2].setPosition(180+140, contenido[2].getPosition().y);
+    contenido[3].setPosition(180+140, contenido[3].getPosition().y);
+    sf::Text* contenido2 = new sf::Text[c_lenght];
+    for (int i=0; i<c_lenght; i++)
+    {
+        contenido2[i].setString(contenido[i].getString());
+        contenido2[i].setFont(fuente);
+        contenido2[i].setCharacterSize(20);
+        contenido2[i].setColor(sf::Color(30,30,30));
+        contenido2[i].setOrigin(1,1);
+        contenido2[i].setPosition(contenido[i].getPosition());
+    }
+
+
+    sf::Text volver = sf::Text();
+    volver.setString("Volver");
+    volver.setFont(fuente);
+    volver.setCharacterSize(40);
+    volver.setColor(sf::Color(180,0,0));
+    volver.setOrigin(volver.getLocalBounds().width/2, volver.getLocalBounds().height/2);
+    volver.setPosition(800,500);
+    sf::Text volver2 = sf::Text();
+    volver2.setString("Volver");
+    volver2.setFont(fuente);
+    volver2.setCharacterSize(40);
+    volver2.setColor(sf::Color(0,0,0));
+    volver2.setOrigin(volver.getLocalBounds().width/2-1, volver.getLocalBounds().height/2-1);
+    volver2.setPosition(volver.getPosition());
+    sf::Sprite m = sf::Sprite(t_marco);
+    m.scale(0.3,0.1);
+    m.setOrigin(marco.getLocalBounds().width/2, marco.getLocalBounds().height/2-110);
+    m.setPosition(volver.getPosition());
+
+
+    bool seguir = true;
+    while (window->isOpen() && seguir)
+    {
+        sf::Event event;
+        while (window->pollEvent(event))
+        {
+            switch(event.type)
+            {
+                case sf::Event::Closed:
+                    window->close();
+                    break;
+                case sf::Event::KeyPressed:
+                // GESTION DE EVENTOS
+                    switch(event.key.code)
+                    {
+                        case sf::Keyboard::Space:
+                        case sf::Keyboard::Return:
+                        case sf::Keyboard::Escape:
+                            // CAMBIAR ESTADO
+                            seguir = false;
+                            break;
+                    }
+                    break;
+            }
+        }
+        window->clear();
+        window->draw(fondo);
+        window->draw(t_sombra);
+        window->draw(t);
+        window->draw(volver2);
+        window->draw(volver);
+        window->draw(m);
+        for(int i=0; i<num_im; i++)
+            window->draw(imagenes[i]);
+        for(int i=1; i<c_lenght;i++)
+            window->draw(contenido2[i]);
+        for(int i=1; i<c_lenght;i++)
+            window->draw(contenido[i]);
+        window->display();
+    }
+    delete[] contenido;
+    contenido = NULL;
+    delete[] contenido2;
+    contenido2 = NULL;
+    delete[] textures;
+    textures = NULL;
+    delete[] imagenes;
+    imagenes = NULL;
+}
+
+int Menu::menuPause(sf::RenderWindow* w)
 {
     // INICIALIZAR VALORES
-    numObjetos = 2;
-    menuObjetos = new sf::Text[numObjetos];
-    int width = window->getSize().x;
-    int height = window->getSize().y;
+    window = w;
+    width = window->getSize().x;
+    height = window->getSize().y;
 
-    menuObjetos[0].setString("CONTINUAR");
-    menuObjetos[1].setString("SALIR");
+    titulo = sf::Text();
+    titulo.setString("Pause");
+    titulo.setFont(fuente);
+    titulo.setCharacterSize(60);
+    titulo.setColor(sf::Color::Yellow);
+    titulo.setOrigin(titulo.getLocalBounds().width/2,titulo.getLocalBounds().height/2);
+    titulo.setPosition(width/2,150);
+    titulo_sombra = sf::Text();
+    titulo_sombra.setString(titulo.getString());
+    titulo_sombra.setFont(fuente);
+    titulo_sombra.setCharacterSize(60);
+    titulo_sombra.setColor(sf::Color(0,0,0));
+    titulo_sombra.setOrigin(titulo_sombra.getLocalBounds().width/2-2,titulo_sombra.getLocalBounds().height/2-2);
+    titulo_sombra.setPosition(titulo.getPosition());
+
+    numObjetos = 3;
+    menuObjetos = new sf::Text[numObjetos];
+    menuObjetos[0].setString("Continuar");
+    menuObjetos[1].setString("Ayuda");
+    menuObjetos[2].setString("Salir");
     for (int i=0; i<numObjetos; i++)
     {
         menuObjetos[i].setFont(fuente);
-        menuObjetos[i].setColor(sf::Color::White);
-        menuObjetos[i].setPosition(sf::Vector2f((width/2)-(menuObjetos[i].getLocalBounds().width/2), height/(numObjetos+1)*(i+1)));
+        menuObjetos[i].setColor(sf::Color(230,230,230));
+        menuObjetos[i].setCharacterSize(40);
+        menuObjetos[i].setOrigin(menuObjetos[i].getLocalBounds().width/2, menuObjetos[i].getLocalBounds().height/2);
+        menuObjetos[i].setPosition(width/2, height/(numObjetos+2)*(i+2));
+    }
+    sf::Text* menuObjetos2 = new sf::Text[numObjetos];
+    for (int i=0; i<numObjetos; i++)
+    {
+        menuObjetos2[i].setString(menuObjetos[i].getString());
+        menuObjetos2[i].setFont(fuente);
+        menuObjetos2[i].setColor(sf::Color(30,30,30));
+        menuObjetos2[i].setCharacterSize(40);
+        menuObjetos2[i].setOrigin(menuObjetos2[i].getLocalBounds().width/2-1, menuObjetos2[i].getLocalBounds().height/2-1);
+        menuObjetos2[i].setPosition(menuObjetos[i].getPosition());
     }
     objetoSeleccionado = 0;
     marcarSeleccionado();
@@ -142,8 +497,10 @@ int Menu::menuPause(sf::RenderWindow* window)
                                     estado->setEstado(1);
                                     break;
                                 case 1:
+                                    ayuda();
+                                    break;
+                                case 2:
                                     //SALIR
-                                    //window->close();
                                     estado->setEstado(0);
                                     break;
                             }
@@ -154,11 +511,31 @@ int Menu::menuPause(sf::RenderWindow* window)
         }
 
         // DIBUJAR
-        dibujar(window);
+        window->clear();
+        window->draw(fondo);
+        //window->draw(fondo_b1);
+        //window->draw(fondo_b2);
+
+
+        window->draw(marco);
+        for (int i=0; i<numObjetos; i++)
+        {
+            window->draw(menuObjetos2[i]);
+        }
+        for (int i=0; i<numObjetos; i++)
+        {
+            window->draw(menuObjetos[i]);
+        }
+        window->draw(titulo_sombra);
+        window->draw(titulo);
+        window->display();
     }
     // eliminar puntero
     delete[] menuObjetos;
     menuObjetos = NULL;
+    delete[] menuObjetos2;
+    menuObjetos2 = NULL;
+    estado = NULL;
 }
 
 Menu::~Menu()
@@ -169,15 +546,16 @@ Menu::~Menu()
 
 void Menu::marcarSeleccionado()
 {
-    menuObjetos[objetoSeleccionado].setColor(sf::Color::Green);
+    menuObjetos[objetoSeleccionado].setColor(sf::Color(180,0,0));
+    marco.setPosition(menuObjetos[objetoSeleccionado].getPosition());
 }
 void Menu::moveUp()
 {
     if(objetoSeleccionado > 0)
     {
-        menuObjetos[objetoSeleccionado].setColor(sf::Color::White);
+        menuObjetos[objetoSeleccionado].setColor(sf::Color(230,230,230));
         --objetoSeleccionado;
-        menuObjetos[objetoSeleccionado].setColor(sf::Color::Green);
+        marcarSeleccionado();
     }
 }
 
@@ -185,17 +563,18 @@ void Menu::moveDown()
 {
     if(objetoSeleccionado < numObjetos-1)
     {
-        menuObjetos[objetoSeleccionado].setColor(sf::Color::White);
+        menuObjetos[objetoSeleccionado].setColor(sf::Color(230,230,230));
         ++objetoSeleccionado;
-        menuObjetos[objetoSeleccionado].setColor(sf::Color::Green);
+        marcarSeleccionado();
     }
 }
-void Menu::dibujar(sf::RenderWindow* window)
+void Menu::dibujar()
 {
     window->clear();
     for (int i=0; i<numObjetos; i++)
     {
         window->draw(menuObjetos[i]);
     }
+    window->draw(titulo);
     window->display();
 }
